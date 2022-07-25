ca93f7f13a00661bbd76f1fcb788c9764866b0f8
Freya Javier <thesleepyunicorn63@gmail.com>
2022-07-25 09:35:48 -0500

    Initial Commit

---
+++ To-Do List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  To-Do List
+//
+//  Created by Yuna on 7/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
