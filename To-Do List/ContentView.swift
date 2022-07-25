58219f75e1f67fe299bfd0677c1f57f5502c069c
Freya Javier <thesleepyunicorn63@gmail.com>
2022-07-25 11:43:59 -0500

    Enabled data persistence

--- To-Do List/ContentView.swift
+++ To-Do List/ContentView.swift
@@ -54,9 +54,10 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct ToDoItem: Identifiable {
+struct ToDoItem: Identifiable, Codable {
     var id = UUID()
     var priority = String()
     var description = String()
     var dueDate = Date()
 }
+
